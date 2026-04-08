17e08c26a82c626014dd8f80c4c3c987488c9479
seros <github@seros.me>
2026-04-08 13:40:59 +0800

    1

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,8 +1,14 @@
 import ui from '@nuxt/ui/vue-plugin'
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
-import router from './router'
 import './assets/css/main.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
 
 const app = createApp(App)
 
