8c09a8d0efc792bd2408b2616dad3f3aed0366d1
seros <github@seros.me>
2026-04-08 11:49:54 +0800

    1

--- packages/renderer/src/pages/index.vue
+++ packages/renderer/src/pages/index.vue
@@ -1,3 +1,71 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const searchTerm = ref('')
+
+// Pages
+const pages = [
+  {
+    label: 'Chat',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/chat')),
+  },
+  {
+    label: 'Music',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/music')),
+  },
+  {
+    label: 'Music1',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/music1')),
+  },
+  {
+    label: 'Setting',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/setting')),
+  },
+  {
+    label: 'Test',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/test')),
+  },
+  {
+    label: 'Recorder',
+    icon: 'i-lucide-globe',
+    onSelect: selectAndClear(() => router.replace('/recorder')),
+  },
+]
+
+// 分组数组
+const groups = computed(() => [
+  {
+    id: 'pages',
+    label: 'Pages',
+    items: pages,
+  },
+])
+
+function selectAndClear(fn: () => void) {
+  return () => {
+    searchTerm.value = ''
+    fn()
+  }
+}
+</script>
+
 <template>
-  <UCheckbox default-value />
+  <div class="bg-white">
+    <UCommandPalette
+      v-model:search-term="searchTerm"
+      :groups="groups"
+      :fuse="{
+        resultLimit: 7,
+        matchAllWhenSearchEmpty: false,
+      }"
+      class="border-2 border-red-500"
+    />
+  </div>
 </template>
