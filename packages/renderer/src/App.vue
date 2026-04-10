0bd709a781e5269b1cb31ecf01fd6d869e88b321
seros <github@seros.me>
2026-04-10 15:19:21 +0800

    1

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { useMagicKeys } from '@vueuse/core'
+import { watch } from 'vue'
+
+const keys = useMagicKeys()
+const key = keys['Alt+`']
+
+watch(() => key?.value, (pressed) => {
+  if (pressed) {
+    console.log('空格键被按下')
+  }
+  else {
+    console.log('空格键抬起')
+  }
+})
+</script>
+
 <template>
   <UApp>
     <RouterView />
