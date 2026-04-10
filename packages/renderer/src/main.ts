4d812d38d64cf2a7c490846f36b02b82c14e0597
seros <github@seros.me>
2026-04-10 14:53:27 +0800

    1

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,9 +1,13 @@
+import lucideIcons from '@iconify-json/lucide/icons.json'
+import { addCollection } from '@iconify/vue'
 import ui from '@nuxt/ui/vue-plugin'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 import './assets/css/main.css'
+
+addCollection(lucideIcons)
 
 const router = createRouter({
   history: createWebHashHistory(),
