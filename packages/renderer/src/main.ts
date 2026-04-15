065ffad5fe6da15afbcbafed5cda33d0cc1983a2
seros <github@seros.me>
2026-04-15 10:31:56 +0800

    1

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -5,7 +5,7 @@ import ui from '@nuxt/ui/vue-plugin'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
-import { handleHotUpdate, routes } from 'vue-router/auto-routes'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 import './assets/css/main.css'
 
@@ -22,8 +22,3 @@ app.use(router)
 app.use(ui)
 
 app.mount('#app')
-
-// This will update routes at runtime without reloading the page
-if (import.meta.hot) {
-  handleHotUpdate(router)
-}
--- packages/renderer/src/pages/setting/music.vue
+++ packages/renderer/src/pages/setting/music.vue
@@ -1,3 +1,56 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const musicLibraryPath = ref('')
+</script>
+
+<route lang="yaml">
+meta:
+  layout: setting
+</route>
+
 <template>
-  <div>音乐设置</div>
+  <UCard>
+    <UForm>
+      <UPageCard
+        title="Profile"
+        description="These informations will be displayed publicly."
+        variant="naked"
+        orientation="horizontal"
+        class="mb-4"
+      >
+        <UButton
+          form="settings"
+          label="Save changes"
+          color="neutral"
+          type="submit"
+          class="w-fit lg:ms-auto"
+        />
+      </UPageCard>
+      <UPageCard>
+        <UFormField
+          label="音乐库位置"
+          description="Will appear on receipts, invoices, and other communication."
+          required
+          class="flex max-sm:flex-col justify-between items-start gap-4"
+        >
+          <UInput
+            autocomplete="off"
+          />
+        </UFormField>
+        <USeparator />
+        <UFormField
+          label="密钥"
+          description="Used to sign in, for email receipts and product updates."
+          required
+          class="flex max-sm:flex-col justify-between items-start gap-4"
+        >
+          <UInput
+            type="email"
+            autocomplete="off"
+          />
+        </UFormField>
+      </UPageCard>
+    </UForm>
+  </UCard>
 </template>
