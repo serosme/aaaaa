0e5a6731c43d5adeed29124bb21e7c97c88a1b01
seros <github@seros.me>
2026-04-25 23:30:56 +0800

    1

--- packages/renderer/nuxt.config.ts
+++ packages/renderer/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/icon'],
   css: ['~/assets/css/main.css'],
   ui: {
     fonts: false,
